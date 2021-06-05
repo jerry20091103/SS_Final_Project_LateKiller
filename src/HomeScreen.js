71dc9efc5ceae6732c2cbe1ab3e64f08d5160b18
陳柏均 <a265589@yahoo.com.tw>
2021-06-05 23:31:09 +0800

    Homescreen fab

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+
+import React  from 'react';
+import{Text} from 'react-native'
+import { Container, Header, View, Button, Icon, Fab ,Content} from 'native-base';
 export default class HomeScreen extends React.Component {
+    constructor(props) {
+        super(props)
+      }
     render() {
     const {navigate} = this.props.navigation;
     return (
-      <Text>HomeScreen</Text>
+        <Container>
+            <Content>
+                <Text>
+                    HomeScreen
+                </Text>
+        </Content>
+            <View style={{ flex: 1 }}>    
+             <Fab
+                active={true}
+                containerStyle={{ }}
+                style={{ backgroundColor: '#5067FF' }}
+                position="bottomRight"
+                 onPress={()=>{}}>
+                <Icon name="add" />
+                </Fab>
+            </View>
+            
+      </Container>
+
     );
     }
   }
