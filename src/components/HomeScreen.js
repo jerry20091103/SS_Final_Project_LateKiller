cea67d24f944628d2a5879c23ddbb52462243faa
陳柏均 <a265589@yahoo.com.tw>
2021-06-09 00:10:46 +0800

    rebuild the project without firebase

---
+++ src/components/HomeScreen.js
@@ -0,0 +1,32 @@
+import React  from 'react';
+import{Text} from 'react-native'
+import { Container, Header, View, Button, Icon, Fab ,Content} from 'native-base';
+export default class HomeScreen extends React.Component {
+    constructor(props) {
+        super(props)
+      }
+    render() {
+    const {navigate} = this.props.navigation;
+    return (
+        <Container>
+            <Content>
+                <Text>
+                    HomeScreen
+                </Text>
+        </Content>
+            <View style={{ flex: 1 }}>    
+             <Fab
+                active={true}
+                containerStyle={{ }}
+                style={{ backgroundColor: '#5067FF' }}
+                position="bottomRight"
+                 onPress={()=>{}}>
+                <Icon name="add" />
+                </Fab>
+            </View>
+            
+      </Container>
+
+    );
+    }
+  }
