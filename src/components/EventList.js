e3b62322f8b306595179e5feed7c38726f5f0ebb
jerry20091103 <jerry20091103@gmail.com>
2021-06-10 18:18:50 +0800

    Finished Home Screen UI.

---
+++ src/components/EventList.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, Text, Image, FlatList, RefreshControl } from 'react-native';
+import { Container, Header, View, Button, Icon, Fab, Content, Body, Thumbnail, TabHeading } from 'native-base';
+import appColors from '../styles/colors.js';
+import moment from 'moment';
+
+export default class EventList extends React.Component {
+    static propTypes = {
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false
+        };
+
+        this.onRefresh = this.onRefresh.bind(this);
+    }
+
+    renderItem = ({ item }) => (
+        <EventListItem id={item.id} title={item.title} time={item.time} goTime={item.goTime} />
+    );
+    
+    // Showcase pull to refresh only
+    // Delete this and get real data
+    onRefresh() {
+        this.setState({
+            loading: true
+        });
+        setTimeout(()=>(this.setState({loading: false})), 2000);
+    }
+    
+
+    render() {
+        return (
+            <View style={{ margin: 10 }}>
+                <FlatList
+                    data={sampleData} // <== put data here, and it will appear like magic (I hope so).
+                    renderItem={this.renderItem}
+                    ListHeaderComponent={<Text style={styles.smallText}> 活動清單</Text>}
+                    refreshControl={
+                        <RefreshControl 
+                            refreshing={this.state.loading} 
+                            onRefresh={this.onRefresh}
+                        />}
+                />
+            </View>
+        );
+    }
+
+}
+
+const EventListItem = ({ id, title, time, goTime }) => (
+    <Button color={appColors.textBlack} style={styles.eventButton}>
+        <View style={{ flex: 1, flexDirection: 'row'}}>
+            {/* title and room number */}
+            <View style={{ flex: 3, margin: 5 }}>
+                <Text style={styles.titleText}>{title}</Text>
+                <Text style={styles.smallText}>{'# ' + id}</Text>
+            </View>
+            {/* time and goTime */}
+            <View style={{ flex: 4, margin: 5}}>
+                <Text style={styles.titleText}>{time}</Text>
+                <Text style={styles.goTimeText}>{convertGoTime(goTime)}</Text>
+            </View>
+        </View>
+    </Button>
+);
+
+function convertGoTime(goTime) {
+    if (goTime < 60)
+        return goTime + ' min ' + ' 抵達';
+    else
+        return moment().add(goTime, 'minutes').format('hh:mm') + '  出發';
+}
+
+
+const styles = StyleSheet.create({
+    smallText: {
+        color: appColors.textGray,
+        fontSize: 14,
+        marginTop: 'auto'
+    },
+
+    titleText: {
+        color: appColors.textBlack,
+        fontSize: 16,
+        marginBottom: 10
+    },
+
+    goTimeText: {
+        color: appColors.textGreen,
+        fontSize: 18,
+        marginTop: 'auto'
+    },
+
+    eventButton: {
+        marginVertical: 10, 
+        borderRadius: 15, 
+        borderWidth: 1, 
+        backgroundColor: 'white', 
+        borderColor: appColors.textBlack,
+        height: 'auto'
+    }
+})
+
+// this data is only for testing!
+// get real data from firebase and DELETE this
+const sampleData = [
+    {
+        id: '000001',               // room id
+        title: '軟實討論',
+        time: moment().calendar(),  // time of the event
+        goTime: 40,                 // time to go, get this from google map
+    },
+    {
+        id: '000102',
+        title: '1234',
+        time: moment().add(1, 'days').calendar(),
+        goTime: 100,
+    },
+    {
+        id: '000132',
+        title: 'An awesome event with a exteremely long title',
+        time: moment().add(3, 'days').calendar(),
+        goTime: 200,
+    },
+    {
+        id: '000178',
+        title: 'Google IO 2021',
+        time: moment.unix(1621270800).calendar(),
+        goTime: 78,
+    },
+    {
+        id: '000187',
+        title: 'WWDC 2021',
+        time: moment.unix(1622998800).calendar(),
+        goTime: 87,
+    },
+
+];
