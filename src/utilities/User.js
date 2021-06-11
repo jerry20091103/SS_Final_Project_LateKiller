5a57eea6926d21d21c3c033b7a74e2d68bb961d3
Brady29655751 <k0862.kimo@gmail.com>
2021-06-11 23:59:05 +0800

    Google Signin works.

--- src/utilities/User.js
+++ src/utilities/User.js
@@ -1,69 +1,91 @@
 /*
-This file deal with "user sign in" and "get user info".
+This file deal with "user sign in/out" and "get user info".
 Only support Google sign in currently.
-Note: Function Not Tested Yet. It may not work.
+You should make sure prepareSignInWithGoogle() is called before google sign in.
+Also, prepareSignInWithGoogle() should only be called once after app starts.
 */
 
-// Let user sign in with google and return the result(user info).
-// Using popup window, not redirected page.
-export function signInWithGoogle() {
-    let info = undefined;
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        console.log(result);
-        info = result;
-        
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        var userinfo = result.user;
+import {
+    GoogleSignin,
+    GoogleSigninButton,
+    statusCodes,
+} from '@react-native-google-signin/google-signin';
 
-    }).catch(function(error) {
-        console.log(error);
-
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        
-        // The email of the user's account used.
-        var email = error.email;
-        
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-
+// This function should only be called once after app starts and before google sign in.
+export function prepareSignInWithGoogle() {
+    GoogleSignin.configure({
+        webClientId: '1052555520682-q8ji8mkah1566r9v3eeuifn5jvjudlvr.apps.googleusercontent.com'
     });
-    return info;
 }
 
-// Let user sign out. Return undefined if success, else return error.
-export function signOut() {
-    let info = undefined;
-    firebase.auth().signOut().then(() => {
-        console.log("Sign out successfully.");
-    }).catch((error) => {
-        console.log("Sign out failed.");
-        info = error;
-    });
-    return info;
-}
-
-// Get current user info, it maybe undefined.
-export function getUser() {
-    let user = firebase.auth().currentUser;
-    if(user) {
-        // User is signed in.
-        console.log(user);
-    } else {
-        // No user.
-        console.log("No user signed in.");
+// Let user sign in with google and return user info.
+export async function signInWithGoogle() {
+    let userInfo = undefined;
+    try {
+        await GoogleSignin.hasPlayServices();
+        userInfo = await GoogleSignin.signIn();
+        //this.setState({ userInfo });
+    } catch (error) {
+        if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+            // user cancelled the login flow
+            console.log("User cancel log in.");
+        } else if (error.code === statusCodes.IN_PROGRESS) {
+            // operation (e.g. sign in) is in progress already
+            console.log("User is signing in.");
+        } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+            // play services not available or outdated
+            console.log("User has no availabe play services.");
+        } else {
+            // some other error happened
+            console.log("Unknown error.");
+        }
     }
-    return user;
+    return userInfo;
 }
 
-// Get userId.
-export function getUid() {
-    var user = getUser();
-    if(user) {
-        return user.uid;
+// Let user sign out.
+// This function also removes this app from user's authorized apps.
+export async function signOutWithGoogle() {
+    try {
+        await GoogleSignin.revokeAccess();
+        await GoogleSignin.signOut();
+        // this.setState({ user: null }); // Remember to remove the user from your app's state as well
+    } catch (error) {
+        console.error(error);
     }
-    return undefined;
+}
+
+// Get current user info.
+// This function can be used in ComponentDidMount.
+export async function getUserInfo() {
+    let userInfo = undefined;
+    try {
+        userInfo = await GoogleSignin.signInSilently();
+        // this.setState({ userInfo });
+    } catch (error) {
+        if (error.code === statusCodes.SIGN_IN_REQUIRED) {
+          // user has not signed in yet
+          console.log("User hasn't signed in.");
+        } else {
+          // some other error
+          console.log("Unknown error.");
+        }
+    }
+    return userInfo;
+}
+
+// Get userId. Return -1 if failed.
+export async function getUid() {
+    var userInfo = await getUserInfo();
+    if(userInfo) {
+        return userInfo.user.id;
+    }
+    return -1;
+}
+
+// This function is used to check if some user is currently signed in.
+// If network error happened, getUserInfo may reject but this function may return true.
+export async function isUserSignedIn() {
+    const isSignedIn = await GoogleSignin.isSignedIn();
+    return isSignedIn;
 }
